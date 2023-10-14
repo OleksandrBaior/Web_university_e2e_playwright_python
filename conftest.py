dcb0fa556dc4b6a77ab0d95fe7fdb8e4820bb263
OleksandrBaior <bayor.olexandr@gmail.com>
2023-10-14 14:37:28 +0100

    add ajax loader

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
+from playwright.sync_api import Page
 
 @pytest.fixture()
-def new_page(page) -> None:
+def new_page(page: Page) -> None:
     page.set_viewport_size(viewport_size={'width': 1920, 'height': 1080})
     yield page
     
--- tests/test_AJAX_LOADER.py
+++ tests/test_AJAX_LOADER.py
@@ -1,1 +1,21 @@
 # Wait for button and click it
+
+from playwright.sync_api import expect, Page
+import pytest
+
+CLICK_ME_BUTTON = '//*[@id="button1"]/p'
+
+class Test_Ajax_Loader:
+
+    @pytest.fixture
+    def test_setup(self, new_page, page: Page) -> None:
+        page.goto('/Ajax-Loader/index.html')
+
+
+    @pytest.mark.ajax
+    def test_wait_ajax_loader(self, test_setup, page: Page) -> None:
+        
+        page.locator(CLICK_ME_BUTTON).wait_for(state="visible")
+        page.locator(CLICK_ME_BUTTON).click()
+        
+        
