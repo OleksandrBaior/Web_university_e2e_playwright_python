0030503c3af0fd73a4089f65f8912cd63227660d
OleksandrBaior <bayor.olexandr@gmail.com>
2023-10-24 20:27:17 +0100

    fix ajax test

--- tests/test_AJAX_LOADER.py
+++ tests/test_AJAX_LOADER.py
@@ -1,21 +1,17 @@
-# Wait for button and click it
-
-from playwright.sync_api import expect, Page
+       
+from playwright.sync_api import Page, expect
 import pytest
 
 CLICK_ME_BUTTON = '//*[@id="button1"]/p'
 
 class Test_Ajax_Loader:
-
     @pytest.fixture
-    def test_setup(self, new_page, page: Page) -> None:
-        page.goto('/Ajax-Loader/index.html')
-
+    def test_setup(self, new_page: Page) -> None:
+        self.page = new_page
+        self.page.goto('/Ajax-Loader/index.html')
 
     @pytest.mark.ajax
-    def test_wait_ajax_loader(self, test_setup, page: Page) -> None:
-        
-        page.locator(CLICK_ME_BUTTON).wait_for(state="visible")
-        page.locator(CLICK_ME_BUTTON).click()
-        
-        
+    def test_wait_ajax_loader(self, test_setup) -> None:
+        self.page.locator(CLICK_ME_BUTTON).wait_for(state="visible")
+        self.page.locator(CLICK_ME_BUTTON).click()
+        expect(self.page.locator(CLICK_ME_BUTTON)).to_contain_text('CLICK ME!')
